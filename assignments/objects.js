6f96dbda38295ea397c0c97bfe2461548f37ac9c
Brandon Green <jailhousejordans@gmail.com>
2019-06-10 14:48:05 -0500

    mvp, I believe

--- assignments/objects.js
+++ assignments/objects.js
@@ -80,7 +80,9 @@ const Antonietta = {
 // 3. Nest a grandchild object in the child object with properties for name and age.  The name will be Sam and the age will be 30
 // 4. Give each of the objects the ability to speak their names using the this keyword.
 
-const parent = {}
+// const parent = {
+//   name: 'susan'
+// }
 
 // Log the parent object's name
 
